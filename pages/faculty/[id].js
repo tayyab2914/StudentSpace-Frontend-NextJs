3f28ea9d503038afce5f2171158e10609685248e
tayyab2914 <tayyab1234rehman@gmail.com>
2025-02-24 21:24:52 +0500

    Fixed issues

--- pages/faculty/[id].js
+++ pages/faculty/[id].js
@@ -10,13 +10,7 @@ export async function getServerSideProps(context) {
     // Replace the URL with your actual API endpoint.
     const res = await axios.get(`https://admin.studentspace.website/feedback/reviews/?faculty_id=${id}`);
     const data = res.data;
-
-    // Redirect on the server-side
     return {
-      redirect: {
-        destination: `https://studentspace.online/faculty/${id}`,
-        permanent: false, // This is a temporary redirect (you can set it to true for permanent redirects)
-      },
       props: {
         faculty: data.faculty, // contains name, designation, image_url, etc.
       },
@@ -28,25 +22,29 @@ export async function getServerSideProps(context) {
 }
 
 export default function FacultyPage({ faculty }) {
-  // Since we perform a server-side redirect, this code will not run for the user.
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      window.location.href = `https://studentspace.online/faculty/${faculty.id}`;
+    }, 1); //  delay
+    return () => clearTimeout(timer);
+  }, [faculty]);
+
   return (
     <>
       <Head>
-        {/* Page Title */}
         <title>{faculty.name}</title>
+        
+        <meta name="description" content={`View reviews for Professor ${faculty.name} on StudentSpace, highlighting teaching quality and expertise.`} />
 
-        {/* Standard description meta */}
-        <meta name="description" content={faculty.designation} />
-
-        {/* Open Graph meta tags for Facebook and other social platforms */}
         <meta property="og:title" content={faculty.name} />
-        <meta property="og:description" content={faculty.designation} />
+        <meta property="og:description"  content={`View reviews on StudentSpace`} />
         <meta property="og:image" content={faculty.image_url} />
         <meta property="og:url" content={`https://www.studentspace.online/faculty/${faculty.id}`} />
 
-        {/* Twitter card meta tags */}
         <meta name="twitter:title" content={faculty.name} />
-        <meta name="twitter:description" content={faculty.designation} />
+        <meta name="twitter:description"  content={`View reviews on StudentSpace`} />
         <meta name="twitter:image" content={faculty.image_url} />
       </Head>
 
